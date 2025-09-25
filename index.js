c2e195ce1e6091e51d952213421477df5061460f
Sujal <sujalb.is24@rvce.edu.in>
2025-09-25 15:39:21 +0530

    "Add and test file upload with multer"

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import {connectDB} from './db/index.js'
 import {app} from './app.js'
 import cors from "cors"
+import {upload} from "./middlewares/multer.middleware.js"
+import { urlencoded } from 'express'
 
 app.use(cors({origin : '*'}))
+app.use(urlencoded())
 
 connectDB().then(()=>{
     app.listen(8000, ()=>{
@@ -12,3 +15,7 @@ connectDB().then(()=>{
     console.error("ERROR :" + err);
 })
 
+app.post("/User", upload.single('profile_pic'), (req, res)=>{
+    console.log(req.file)
+    res.send("hi");
+})
